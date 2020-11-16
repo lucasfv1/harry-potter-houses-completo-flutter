9ad71b9e65182f336abe8e8a10d3248d3382ab91
Lucas <lucasfagundesvaz@gmail.com>
2020-11-16 13:54:24 -0300

    Passo 2 - Solução.

--- lib/question.dart
+++ lib/question.dart
@@ -1,5 +1,7 @@
 //TODO: Passo 2 - Crie uma nova classe chamada Question.
 
+class Question{}
+
 //TODO: Passo 3 - Crie três propriedades para essa classe: a) questionTitle para armazenar o texto da questão; b)choice1 para armazenar o texto relativo à opção 1; c) choice2 para armazenar o texto relativo à opção 2.
 
 //TODO: Passo 4 - Crie um construtor para essa classe para que seja possível inicializar as propriedades criadas na etapa 3.
